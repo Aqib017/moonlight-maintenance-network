e463e53601e0232c51317802b8cbf2027baca31c
Aqib017 <aqib1227dilshad@gmail.com>
2026-06-05 10:05:06 +0530

    Added Swagger JWT authorization support

---
+++ moonlight-maintenance/src/main/java/com/moonlight/mnt/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.moonlight.mnt.controller;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.moonlight.mnt.dto.UserRequest;
+import com.moonlight.mnt.dto.UserResponse;
+import com.moonlight.mnt.service.UserService;
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	@PostMapping
+	public UserResponse createUser(@RequestBody UserRequest request) {
+		return userService.createUser(request);
+	}
+	@GetMapping
+	public List<UserResponse> getAllUsers() {
+		return userService.getAllUsers();
+	}
+}
---
+++ moonlight-maintenance/src/main/java/com/moonlight/mnt/service/UserService.java
@@ -0,0 +1,32 @@
+package com.moonlight.mnt.service;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.moonlight.mnt.dto.UserRequest;
+import com.moonlight.mnt.dto.UserResponse;
+import com.moonlight.mnt.entity.User;
+import com.moonlight.mnt.repository.UserRepository;
+@Service
+public class UserService {
+	
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+
+	public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+	public UserResponse createUser(UserRequest request) {
+		User user = new User();
+		user.setUsername(request.getUsername());
+		user.setPassword(passwordEncoder.encode(request.getPassword()));
+		User savedUser = userRepository.save(user);
+		return new UserResponse(savedUser.getId(), savedUser.getUsername());
+	}
+	public List<UserResponse> getAllUsers() {
+		return userRepository.findAll().stream()
+										.map(user ->new UserResponse(user.getId(), user.getUsername()))
+										.collect(Collectors.toList());
+	}
+}
